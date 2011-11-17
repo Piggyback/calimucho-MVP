78ea4e0f98f68206fc3e15dd436abf238b9837b5
Kimberly Hsiao <kimberly.hsiao@gmail.com>
2011-11-16 20:16:16 -0800

    new view for refer friends with button at bottom

---
+++ CalimuchoParse/ReferFriendsVC.h
@@ -0,0 +1,13 @@
+//
+//  ReferFriendsVC.h
+//  CalimuchoParse
+//
+//  Created by Kimberly Hsiao on 11/16/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReferFriendsVC : UIViewController
+
+@end
